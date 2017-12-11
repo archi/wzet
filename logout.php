30fd4df04445dfa1566f4ebc33db4dfac1752cac
Sebastian Meyer <ich@sebmeyer.de>
2017-12-11 13:22:52 +0100

    fix logout

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
-setcookie ("kochen_uid", 0, -1, "/~sebastian/");
-setcookie ("kochen_sid", 0, -1, "/~sebastian/");
+include ('inc/config.php');
+setcookie ("kochen_uid", 0, -1, $_CONFIG["COOKIE_PATH"]);
+setcookie ("kochen_sid", 0, -1, $_COOKIE["COOKIE_PATH"]);
 unset ($_COOKIE['kochen_uid']);
 unset ($_COOKIE['kochen_sid']);
 print ("Ausgeloggt!<br><br>");
